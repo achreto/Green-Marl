3c11fee1bf8b3060dbea002a4b6475deb5a351f0
Reto Achermann <rachermann@vmware.com>
2018-09-10 13:29:14 -0700

    make the initialization deterministic
    Signed-off-by: Reto Achermann <rachermann@vmware.com>

--- apps/output_cpp/gm_graph/inc/gm_internal.h
+++ apps/output_cpp/gm_graph/inc/gm_internal.h
@@ -7,10 +7,10 @@
 //-------------------------------------------------------------------------------
 // If the following flag is on, we let the correct thread 'touches' the data strcutre
 // for the first time, so that the memory is allocated in the corresponding socket.
-#define GM_GRAPH_NUMA_OPT   1   
+//#define GM_GRAPH_NUMA_OPT   1   
 
 // defines the size of edge_t and node_t
-#include "gm_graph_typedef.h"  
+#include "gm_graph_typedef.h"
 
 // include the correct platform dependency
 #if defined(__x86_64__) || defined(__i386__)
@@ -20,10 +20,10 @@
   #if defined (__ORACLE__)
    #include "../platform/sparc/inc/gm_platform_helpers.h"
   #else
-   #error "We need __ORACLE__ Sparc environment" 
+   #error "We need __ORACLE__ Sparc environment"
   #endif
  #else
-   #error "We need x86 (32bit or 64bit) or Sparc environment" 
+   #error "We need x86 (32bit or 64bit) or Sparc environment"
  #endif
 #endif
 
